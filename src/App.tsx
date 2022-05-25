9dc35eef6586e0c7c25edf6f7d1cffbff7c10612
AlbSag <92945087+TryoSag@users.noreply.github.com>
2022-05-25 22:57:47 +0200

    9 errores 4app 1list 2get 2 ship comentada funcion recuento

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from "react";
 import { useEffect } from "react";
 import { ShipsList } from "./components/ShipList/ShipList";
 import { IShipsInformation } from "./typeSchema/shipsInformation";
 import { getShips } from "./utils/getShips";
 
 function App() {
-  useEffect()= async()=>{
-  const shipsClassList: string[] = getShips();
-  },[]
-
-  const shipsInformation: IShipsInformation[] = shipsClassList.reduce(
-    (accumulator, class) => {
-      return { ...accumulator, [class]: (accumulator[class] || 0) + 1 };
-    },
-    {}
-  );
+  debugger;
+  const shipsClassList: Promise<string[]> = getShips();
+  console.log(shipsClassList);
+  //const shipsInformation = shipsClassList.reduce((accumulator, class) => { ...accumulator, [class]: (accumulator[class] || 0) + 1 } ,{});
 
   return (
     <div className="App">
-      <ShipsList shipsInformation={shipsInformation} />
+      <ShipsList shipsInformation={shipsClassList} />
     </div>
   );
 }
--- src/utils/getShips.tsx
+++ src/utils/getShips.tsx
@@ -1,14 +1,17 @@
 import axios from "axios";
 
 export const getShips = async () => {
-  let shipsClassList: string[];
+  let shipsClassList: string[] = [];
 
   for (let i = 0; i < 4; i++) {
     const {
       data: { results },
     } = await axios.get(`https://swapi.dev/api/starships/?page=${i + 1}`);
 
-    results.map((result) => [...shipsClassList, result.starship_class]);
+    results.map((result: { starship_class: string }) => [
+      ...shipsClassList,
+      result.starship_class,
+    ]);
   }
 
   return shipsClassList;
